031010774b8c3d5c3decf7a6c84283d7991bc863
Chansoo Song <chansoo.song@ag.ny.gov>
2022-02-13 10:10:31 -0500

    edit docstrings for base class layout

--- oagdedupe/base.py
+++ oagdedupe/base.py
@@ -10,28 +10,28 @@ class BaseBlocker(metaclass=ABCMeta):
     def ...
 
 class BaseBlockAlgos(metaclass=ABCMeta):
-    """ Abstract base class for all blockers to inherit
+    """ Abstract base class for all blocking algos to inherit
     """
 
     @abstractmethod
     def ...
 
 class BaseDistance(metaclass=ABCMeta):
-    """ Abstract base class for all blockers to inherit
+    """ Abstract base class for all distance configurations to inherit
     """
 
     @abstractmethod
     def ...
 
 class BaseTrain(metaclass=ABCMeta):
-    """ Abstract base class for all blockers to inherit
+    """ Abstract base class for all training algos to inherit
     """
 
     @abstractmethod
     def ...
 
 class BaseCluster(metaclass=ABCMeta):
-    """ Abstract base class for all blockers to inherit
+    """ Abstract base class for all clustering algos to inherit
     """
 
     @abstractmethod
