20db337b7f649849d4f24316a8853d6c999e16ee
Chansoo Song <chansoo.song@ag.ny.gov>
2022-02-13 10:25:55 -0500

    draft 1 of layout complete

--- oagdedupe/block/blockers.py
+++ oagdedupe/block/blockers.py
@@ -2,7 +2,8 @@ from typing import List, Union, Any, Optional, Dict
 from dataclasses import dataclass
 from oagdedupe.base import BaseBlocker
 
-class ManualBlocker(BaseBlocker):
+class TestBlocker(BaseBlocker):
 
+class ManualBlocker(BaseBlocker):
 
 class AutoBlocker(BaseBlocker):
--- oagdedupe/model.py
+++ oagdedupe/model.py
@@ -17,29 +17,33 @@ class BaseModel(metaclass=ABCMeta):
     cluster = ConnectedComponents()
     
     @abstractmethod
-    def predict(self,df, cols):
+    def predict(self, df, cols):
         """
-        Use trained model to identify matched candidates.
-        Use clustering algorithm to assign cluster IDs. Default is to define each connected component as a cluster.
-        Handle unclustered nodes.
-        Returns cluster IDs
+        (1) Use trained model to identify matched candidates.
+        (2) Use clustering algorithm to assign cluster IDs. Default is to define each connected component as a cluster.
+        (3) Handle unclustered nodes.
+        (4) Returns cluster IDs
         """
         return
 
     @abstractmethod    
     def train():
         """
-        Computes similarity scores for each column.
-        Then fits a model to learn p(match).
-        Default model is to average similarity scores and use a threshold of 0.9.
-        Return model that takes any dataframe or dataframes with same columns, and returns matched candidates.
+        (1) Computes similarity scores for each column.
+        (2) fit a model to learn p(match).
+            - Default model is to average similarity scores and use a threshold of 0.9.
+        (3) Return model that takes any dataframe or dataframes with same columns, and returns matched candidates.
         """
         return
 
     @abstractmethod
     def get_candidates(self):
         """
-        Returns candidate pairs of records. Blocking is optional.
+        1) generate unique IDs;
+        2) check if blocker selected
+            - if blocker not selected, block map is all possible combinations of ID pairs;
+            - else use blocker to get block map;
+        3) use block map to generate pairs of candidate pairs of records
         """
         return
 
@@ -48,10 +52,11 @@ class Dedupe(BaseModel, BlockerMixin):
     """General dedupe block, inherits from BaseModel.
     """
 
-    def predict(self,df, cols):
+    def predict(self, df, cols):
         return
 
     def train(self):
+
         return
 
     def get_candidates(self):
@@ -62,7 +67,7 @@ class RecordLinkage(BaseModel, BlockerMixin):
     """General record linkage block, inherits from BaseModel.
     """
 
-    def predict(self,df, cols):
+    def predict(self, df, cols):
         return
 
     def train():
