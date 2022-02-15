51eac48720509255bd2025749ae65af328e8ecec
Chansoo Song <chansoo.song@ag.ny.gov>
2022-02-14 21:29:14 -0500

    refactoring blocking and add rl get candidates test

--- oagdedupe/mixin.py
+++ oagdedupe/mixin.py
@@ -2,9 +2,9 @@ from typing import List, Union, Any, Set, Optional, Dict
 from dataclasses import dataclass
 import itertools
 
+
 @dataclass
 class BlockerMixin:
-
     def product(self, lists, nodupes=False):
         result = [[]]
         for item in lists:
@@ -15,22 +15,26 @@ class BlockerMixin:
         return result
 
     def dedupe_get_candidates(self, block_maps):
-        return set([
-            x
-            for block_map in block_maps
-            for ids in block_map.values()
-            for x in itertools.combinations(ids, 2)
-        ])
-    
-    def joint_keys(self,dict1,dict2): 
-        return [
-            name for name in set(dict1).intersection(set(dict2))
-        ]
+        return set(
+            [
+                x
+                for block_map in block_maps
+                for ids in block_map.values()
+                for x in itertools.combinations(ids, 2)
+            ]
+        )
+
+    def joint_keys(self, dict1, dict2):
+        return [name for name in set(dict1).intersection(set(dict2))]
 
     def rl_get_candidates(self, block_maps1, block_maps2):
-        return set([
-            tuple(pair)
-            for block_map1, block_map2 in zip(block_maps1, block_maps2)
-            for key in self.joint_keys(block_map1,block_map2)
-            for pair in self.product([block_map1[key], block_map2[key]], nodupes=False)
-        ])
+        return set(
+            [
+                tuple(pair)
+                for block_map1, block_map2 in zip(block_maps1, block_maps2)
+                for key in self.joint_keys(block_map1, block_map2)
+                for pair in self.product(
+                    [block_map1[key], block_map2[key]], nodupes=False
+                )
+            ]
+        )
