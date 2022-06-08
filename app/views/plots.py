feb72b814d14cc7fa45d545e3fa8c240d25436d0
Chansoo Song <chansoo.song@ag.ny.gov>
2022-06-08 10:11:56 -0400

    redirect to load page if dataset not loaded; and edit so that candidate deque is not popped on refresh

--- app/views/plots.py
+++ app/views/plots.py
@@ -1,13 +1,16 @@
 from .. import app
 from .. import utils
 
-from flask import render_template
+from flask import (
+    render_template,
+    redirect
+)
 
 @app.route('/plots', methods=["GET","POST"])
 def load_plots():
 
     if not hasattr(app.init, "d"):
-        app.init._load_dataset(app.cached_files[0], app.lab)
+        return redirect('/load/nodata')
 
     scatterplt, kdeplot = utils.get_plots(
         X=app.init.d.trainer.X,
