39617dc1a7caad0f48fdf359ca6cdb49cb46cd04
Chansoo Song <chansoo.song@ag.ny.gov>
2022-08-03 17:59:49 -0400

    update fastapi

--- dedupe/fastapi/main.py
+++ dedupe/fastapi/main.py
@@ -37,29 +37,29 @@ async def payload(data:Union[u.Annotation, None]):
     m.generate_new_samples()
 
 # below are not used in active learning loop
-@app.get("/samples")
-async def get_samples(n_instances:int=10):
-    query_index, samples = m.get_samples(n_instances=n_instances)
-    return dict({
-        "query_index": query_index.tolist(),
-        "samples":samples.to_dict()
-    })
+# @app.get("/samples")
+# async def get_samples(n_instances:int=10):
+#     query_index, samples = m.get_samples(n_instances=n_instances)
+#     return dict({
+#         "query_index": query_index.tolist(),
+#         "samples":samples.to_dict()
+#     })
 
-@app.post("/submit")
-async def submit_labels(labels: u.Query):
-    samples = pd.DataFrame(labels.samples)
-    samples["idx"] = labels.query_index
-    samples.to_sql(
-        "labels", 
-        con=m.engine, 
-        if_exists="append", 
-        index=False
-    )
+# @app.post("/submit")
+# async def submit_labels(labels: u.Query):
+#     samples = pd.DataFrame(labels.samples)
+#     samples["idx"] = labels.query_index
+#     samples.to_sql(
+#         "labels", 
+#         con=m.engine, 
+#         if_exists="append", 
+#         index=False
+#     )
 
-@app.get("/get_labels")
-async def get_labels():
-    df = pd.read_sql("SELECT * FROM labels", con=m.engine).drop_duplicates()
-    return df.to_dict()
+# @app.get("/get_labels")
+# async def get_labels():
+#     df = pd.read_sql("SELECT * FROM labels", con=m.engine).drop_duplicates()
+#     return df.to_dict()
 
 if __name__=="__main__":
     uvicorn.run(
--- dedupe/fastapi/utils.py
+++ dedupe/fastapi/utils.py
@@ -26,38 +26,9 @@ class Annotation(BaseModel):
     annotation: dict
     project: dict
 
-class LabelStudioConnection:
+class Tasks:
 
-    def __init__(self):
-        self.lsapi = LabelStudioAPI()
-
-    def check_project_exists(self):
-        for proj in self.lsapi.list_projects()["results"]:
-            if proj["title"] == self.title:
-                return proj
-        return False
-
-    def setup_project(self):
-        proj = self.check_project_exists()
-        if not proj:
-            new_proj = self.lsapi.create_project(title=self.title, description=self.description)
-            return new_proj
-        return proj
-
-    def post_tasks(self):
-        query_index, df = self.get_samples(n_instances=10)
-        df["idx"] = query_index
-        df[["idx"]].to_sql("query_index", con=self.engine, if_exists="append", index=False)
-        self.lsapi.post_tasks(df=df, project_id=self.proj["id"])
-        return
-        
     def generate_new_samples(self):
-        """
-        1. listens for labelstudio with specified project title
-        2. pulls count of incomplete tasks
-        3. if count is < n; pull new samples
-        """
-
         if self.proj["task_number"] is None:
             self.post_tasks()
         else:
@@ -66,8 +37,14 @@ class LabelStudioConnection:
             if  n_incomplete < 5:
                 # train and get new samples
                 self.get_annotations()
-                self.train()
                 self.post_tasks()
+
+    def post_tasks(self):
+        query_index, df = self.get_samples(n_instances=10)
+        df["idx"] = query_index
+        df[["idx"]].to_sql("query_index", con=self.engine, if_exists="append", index=False)
+        self.lsapi.post_tasks(df=df, project_id=self.proj["id"])
+        return
 
     def get_annotations(self):
         annotations = self.lsapi.get_all_annotations(project_id=self.proj["id"])
@@ -78,6 +55,7 @@ class LabelStudioConnection:
         ]
         df = pd.DataFrame(tasks, columns = ["label"] + self.attributes_l_r + ["idx"])
         df["label"] = df["label"].map(self.label_map)
+        self.train(df)
         df.to_sql("labels", con=self.engine, if_exists="append", index=False)
 
     @property
@@ -87,62 +65,6 @@ class LabelStudioConnection:
             "Not a Match":0,
             "Uncertain":2
         }
-
-class Model(LabelStudioConnection):
-
-    def __init__(self):
-        super().__init__()
-
-        logging.info(f'reading database: {config.cache_fp}')
-        self.engine = create_engine(f"sqlite:///{config.cache_fp}", echo=True)
-
-        if os.path.exists(config.model_fp):
-            self.estimator = joblib.load(config.model_fp)
-            logging.info(f'reading model: {config.model_fp}')
-        else:
-            self.estimator = RandomForestClassifier()
-
-        # check for new project; create if not exist
-        self.title = config.ls_title
-        self.description = config.ls_description
-        self.proj = self.setup_project()
-        if not self.lsapi.get_webhooks():
-            self.lsapi.post_webhook(project_id=self.proj["id"])
-    
-    @cached_property
-    def X(self):
-        return pd.read_sql_query("select * from distances", con=self.engine)
-
-    @cached_property
-    def attributes(self):
-        return list(
-            pd.read_sql_query("select * from df limit 1", con=self.engine)
-            .drop("idx",axis=1).columns
-        )
-
-    @property
-    def attributes_l_r(self):
-        return [x+"_l" for x in self.attributes] + [x+"_r" for x in self.attributes]
-
-    @cached_property
-    def clf(self):
-        clf = ActiveLearner(
-            estimator=self.estimator,
-            query_strategy=uncertainty_sampling
-        )
-        clf.teach(np.repeat(1, len(self.attributes)).reshape(1, -1), [1])
-        return clf
-
-    def train(self):
-        df = pd.read_sql("""
-            SELECT idx,label FROM labels
-            WHERE label in (0, 1)
-        """, con=self.engine).drop_duplicates()
-
-        self.clf.teach(
-            X = self.X.loc[list(df["idx"])],
-            y = df["label"]
-        )
 
     def get_samples(self, n_instances=5):
 
@@ -179,6 +101,79 @@ class Model(LabelStudioConnection):
         samples.columns = self.attributes_l_r
 
         return query_index, samples
+    
+
+class Projects:
+
+    def check_project_exists(self):
+        for proj in self.lsapi.list_projects()["results"]:
+            if proj["title"] == self.title:
+                return proj
+        return False
+
+    def setup_project(self):
+        proj = self.check_project_exists()
+        if not proj:
+            new_proj = self.lsapi.create_project(title=self.title, description=self.description)
+            return new_proj
+        return proj
+
+class Database:
+
+    @cached_property
+    def X(self):
+        return pd.read_sql_query("select * from distances", con=self.engine)
+
+    @cached_property
+    def attributes(self):
+        return list(
+            pd.read_sql_query("select * from df limit 1", con=self.engine)
+            .drop("idx",axis=1).columns
+        )
+
+    @property
+    def attributes_l_r(self):
+        return [x+"_l" for x in self.attributes] + [x+"_r" for x in self.attributes]
+
+class Model(Tasks, Projects):
+
+    def __init__(self):
+
+        self.lsapi = LabelStudioAPI()
+        
+        logging.info(f'reading database: {config.cache_fp}')
+        self.engine = create_engine(f"sqlite:///{config.cache_fp}", echo=True)
+
+        if os.path.exists(config.model_fp):
+            self.estimator = joblib.load(config.model_fp)
+            logging.info(f'reading model: {config.model_fp}')
+        else:
+            self.estimator = RandomForestClassifier()
+
+        # check for new project; create if not exist
+        self.title = config.ls_title
+        self.description = config.ls_description
+        self.proj = self.setup_project()
+        if not self.lsapi.get_webhooks():
+            self.lsapi.post_webhook(project_id=self.proj["id"])
+
+    
+    @cached_property
+    def clf(self):
+        clf = ActiveLearner(
+            estimator=self.estimator,
+            query_strategy=uncertainty_sampling
+        )
+        clf.teach(np.repeat(1, len(self.attributes)).reshape(1, -1), [1])
+        return clf
+
+    def train(self, df):
+        self.clf.teach(
+            X = self.X.loc[list(df["idx"])],
+            y = df["label"]
+        )
+
+    
 
 def url_checker(url):
     try:
