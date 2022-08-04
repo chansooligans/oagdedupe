92da5fe5d9879a7b6a8b9d92129fe0ad7bfda4c1
Chansoo Song <chansoo.song@ag.ny.gov>
2022-08-04 14:35:20 -0400

    try adjusting dir

--- dedupe/__init__.py
+++ dedupe/__init__.py
@@ -1,5 +1,8 @@
 import os
-DIR = os.path.dirname(os.path.abspath(__file__)).rsplit("/", 1)[0]
+# DIR = os.path.dirname(os.path.abspath(__file__)).rsplit("/", 1)[0]
+
+DIR = os.path.expanduser("~")
+print(DIR)
 
 from dedupe.config import Config
 config = Config()
