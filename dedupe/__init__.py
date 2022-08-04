292f0f7ff2de52550372567ec48923d3b7e9cbeb
Chansoo Song <chansoo.song@ag.ny.gov>
2022-08-04 19:16:14 -0400

    allow installer to modify configuration

--- dedupe/__init__.py
+++ dedupe/__init__.py
@@ -1,8 +1,4 @@
 import os
-# DIR = os.path.dirname(os.path.abspath(__file__)).rsplit("/", 1)[0]
-
-DIR = os.path.expanduser("~")
-print(DIR)
 
 from dedupe.config import Config
 config = Config()
--- dedupe/config.py
+++ dedupe/config.py
@@ -1,11 +1,62 @@
 import configparser
-from dedupe import DIR
+import os
+import logging
+
+CONFIG_FOLDER_NAME = ".dedupe"
+
+def get_dedupe_file_path(*filepath) -> str:
+    home = os.path.expanduser("~")
+    if home is None:
+        raise RuntimeError("No home directory.")
+
+    return os.path.join(home, CONFIG_FOLDER_NAME, *filepath)
+
+def get_project_dedupe_file_path(*filepath):
+    """Return the full path to a filepath in ${CWD}/.dedupe.
+    """
+    return os.path.join(os.getcwd(), CONFIG_FOLDER_NAME, *filepath)
+
+CONFIG_FILENAMES = [
+    get_dedupe_file_path("config.ini"),
+    get_project_dedupe_file_path("config.ini"),
+]
 
 class Config:
 
     def __init__(self):
+        
         self.config = configparser.ConfigParser()
-        self.config.read(f"{DIR}/.dedupe/.config")
+        
+        for filename in CONFIG_FILENAMES:
+            if os.path.exists(filename):
+                self.config.read(filename)
+                break
+        
+        if self.config.sections() != ['MAIN', 'LABEL_STUDIO', 'FAST_API']:
+            logging.error(
+                """
+                A config file is required, there are two options:
+
+                (1) A global config file at ~/.dedupe/config.ini
+                (2) A per-project config file at $CWD/.dedupe/config.toml, where $CWD is the folder you're running your script from (the folder where you `import dedupe`).
+
+                The config file should contain:
+
+                [MAIN]
+                HOST = http://172.22.39.26
+                MODEL_FILEPATH = [path to model to load, or save if it does not exist]
+                CACHE_FILEPATH = [path to store database files for SQLite and comparison pairs]
+
+                [LABEL_STUDIO]
+                PORT = 8001
+                API_KEY = [API KEY from Label Studio interface after signing up / logging in]
+                TITLE = [Title of Dedupe Project]
+                DESCRIPTION = [Description of Project]
+
+                [FAST_API]
+                PORT = 8000
+                """
+            )
 
     @property
     def host(self):
