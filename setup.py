583dca771814d610f8b6566c315c550e7cc030e8
Chansoo Song <chansoo.song@ag.ny.gov>
2022-08-04 11:19:57 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 import setuptools
 
+requirements, dependency_links = [], []
+with open('requirements.txt') as f:
+    for line in f.read().splitlines():
+        if line.startswith('-e git+'):
+            dependency_links.append(line.replace('-e ', ''))
+        else:
+            requirements.append(line)
+
 setuptools.setup(
     name="dedupe",  # name of your python package
-    version="1.0",
+    version="2.0",
     author="Chansoo Song",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
+    dependency_links=dependency_links
 )
