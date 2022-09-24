ca99c90e450821ef71b2ca34f0cf82915cec0c7c
Chansoo Song <chansoosong@chansoos-air.myfiosgateway.com>
2022-09-24 10:32:26 -0400

    testing myst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'oagdedupe'
+author = 'Chansoo Song, Gautam Sisodia'
 
 release = '0.1'
 version = '0.1.0'
@@ -15,8 +14,11 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
