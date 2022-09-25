dff2e92ec7870f39a3ef88a8933475836e72de5c
Chansoo Song <chansoosong@chansoos-air.myfiosgateway.com>
2022-09-25 13:40:30 -0400

    add license

--- setup.py
+++ setup.py
@@ -11,7 +11,12 @@ with open('requirements.txt') as f:
 setuptools.setup(
     name="oagdedupe",  # name of your python package
     version="2.0",
-    author="Chansoo Song",
+    author="Chansoo Song, Gautam Sisodia",
+    license="MIT",
+    url="https://github.com/chansooligans/oagdedupe",
+    repository="https://github.com/chansooligans/oagdedupe",
+    documentation="https://deduper.readthedocs.io/en/latest/",
+    keywords=["dedupe", "entity resolution", "record linkage", "blocking"],
     packages=setuptools.find_packages(),
     install_requires=requirements,
     dependency_links=dependency_links
