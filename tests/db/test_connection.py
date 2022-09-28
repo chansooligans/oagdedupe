9b7815869393a9cea59d53782930a9c65da406d4
Chansoo Song <chansoo.song@ag.ny.gov>
2022-09-28 12:51:53 -0400

    test db connection on github workflow

---
+++ tests/db/test_connection.py
@@ -0,0 +1,21 @@
+import pytest
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine(
+    "postgresql+psycopg2://username:password@0.0.0.0:8000/db")
+Session = scoped_session(sessionmaker(bind=engine))
+Base = declarative_base()
+
+@pytest.fixture(scope="module")
+def db_session():
+    Base.metadata.create_all(engine)
+    session = Session()
+    yield session
+    session.close()
+    Base.metadata.drop_all(bind=engine)
+
+def test_connection(db_session):
+    res = db_session.query(text("1"))
+    assert res.all() == [(1,)]
