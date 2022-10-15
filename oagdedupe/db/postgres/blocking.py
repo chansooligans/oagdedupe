cf0d05dbd2a9444164b9d8ccac752f072108362f
Chansoo Song <chansoo.song@ag.ny.gov>
2022-10-14 20:48:39 -0400

    mixin for non required funcs

--- oagdedupe/db/postgres/blocking.py
+++ oagdedupe/db/postgres/blocking.py
@@ -21,90 +21,8 @@ from oagdedupe.settings import Settings
 
 
 @dataclass
-class PostgresBlocking(BaseComputeBlocking, BlockSchemes):
-    settings: Settings = Provide[Container.settings]
-
-    def query(self, sql: str) -> pd.DataFrame:
-        """
-        for parallel implementation, need to create separate engine
-        for each process
-        """
-        engine = create_engine(self.settings.db.path_database)
-        res = pd.read_sql(sql, con=engine)
-        engine.dispose()
-        return res
-
-    @du.recordlinkage_both
-    def n_df(self, rl: str = "") -> pd.DataFrame:
-        return self.query(
-            f"SELECT count(*) FROM {self.settings.db.db_schema}.df{rl}"
-        )["count"].values[0]
-
-    @cached_property
-    def n_comparisons(self) -> float:
-        """number of total possible comparisons"""
-        n = self.n_df()
-        if not self.settings.model.dedupe:
-            return np.product(n)
-        return (n * (n - 1)) / 2
-
-    @property
-    def min_rr(self) -> float:
-        """minimum reduction ratio"""
-        reduced = self.n_comparisons - self.settings.model.max_compare
-        return reduced / self.n_comparisons
-
-    @du.recordlinkage
-    def _pairs_query(self, names: Tuple[str], rl: str = "") -> str:
-        if rl == "":
-            where = "WHERE t1._index_l < t2._index_r"
-        else:
-            where = ""
-        return f"""
-            SELECT _index_l, _index_r
-            FROM inverted_index t1
-            JOIN inverted_index_link t2
-                ON {" and ".join(
-                    [f"t1.{s} = t2.{s}" for s in self._aliases(names)]
-                )}
-            {where}
-            GROUP BY _index_l, _index_r
-            """
-
-    def check_unnest(self, name):
-        if "ngrams" in name:
-            return f"unnest({name})"
-        return name
-
-    def signatures(self, names):
-        return ", ".join(
-            [
-                f"{self.check_unnest(name)} as signature{i}"
-                for i, name in enumerate(names)
-            ]
-        )
-
-    def _aliases(self, names: Tuple[str]) -> List[str]:
-        return [f"signature{i}" for i in range(len(names))]
-
-    def _inv_idx_query(
-        self, names: Tuple[str], table: str, col: str = "_index_l"
-    ) -> str:
-        return f"""
-        SELECT
-            {self.signatures(names)},
-            unnest(ARRAY_AGG(_index ORDER BY _index asc)) {col}
-        FROM {self.settings.db.db_schema}.{table}
-        GROUP BY {", ".join(self._aliases(names))}
-        """
-
-    @property
-    def comptab_map(self) -> Dict[str, str]:
-        return {"blocks_train": "comparisons", "blocks_df": "full_comparisons"}
-
-    ########################################################################
-    # forward
-    ########################################################################
+class BlockingMixin:
+    """convenience funcs particular to postgres"""
 
     def query_blocks(self, table: str, columns: List[str]) -> str:
         """
@@ -186,6 +104,93 @@ class PostgresBlocking(BaseComputeBlocking, BlockSchemes):
             );
         """
         )
+
+    def query(self, sql: str) -> pd.DataFrame:
+        """
+        for parallel implementation, need to create separate engine
+        for each process
+        """
+        engine = create_engine(self.settings.db.path_database)
+        res = pd.read_sql(sql, con=engine)
+        engine.dispose()
+        return res
+
+    @du.recordlinkage_both
+    def n_df(self, rl: str = "") -> pd.DataFrame:
+        return self.query(
+            f"SELECT count(*) FROM {self.settings.db.db_schema}.df{rl}"
+        )["count"].values[0]
+
+    @cached_property
+    def n_comparisons(self) -> float:
+        """number of total possible comparisons"""
+        n = self.n_df()
+        if not self.settings.model.dedupe:
+            return np.product(n)
+        return (n * (n - 1)) / 2
+
+    @property
+    def min_rr(self) -> float:
+        """minimum reduction ratio"""
+        reduced = self.n_comparisons - self.settings.model.max_compare
+        return reduced / self.n_comparisons
+
+    @du.recordlinkage
+    def _pairs_query(self, names: Tuple[str], rl: str = "") -> str:
+        if rl == "":
+            where = "WHERE t1._index_l < t2._index_r"
+        else:
+            where = ""
+        return f"""
+            SELECT _index_l, _index_r
+            FROM inverted_index t1
+            JOIN inverted_index_link t2
+                ON {" and ".join(
+                    [f"t1.{s} = t2.{s}" for s in self._aliases(names)]
+                )}
+            {where}
+            GROUP BY _index_l, _index_r
+            """
+
+    def check_unnest(self, name):
+        if "ngrams" in name:
+            return f"unnest({name})"
+        return name
+
+    def signatures(self, names):
+        return ", ".join(
+            [
+                f"{self.check_unnest(name)} as signature{i}"
+                for i, name in enumerate(names)
+            ]
+        )
+
+    def _aliases(self, names: Tuple[str]) -> List[str]:
+        return [f"signature{i}" for i in range(len(names))]
+
+    def _inv_idx_query(
+        self, names: Tuple[str], table: str, col: str = "_index_l"
+    ) -> str:
+        return f"""
+        SELECT
+            {self.signatures(names)},
+            unnest(ARRAY_AGG(_index ORDER BY _index asc)) {col}
+        FROM {self.settings.db.db_schema}.{table}
+        GROUP BY {", ".join(self._aliases(names))}
+        """
+
+    @property
+    def comptab_map(self) -> Dict[str, str]:
+        return {"blocks_train": "comparisons", "blocks_df": "full_comparisons"}
+
+
+@dataclass
+class PostgresBlocking(BaseComputeBlocking, BlockingMixin, BlockSchemes):
+    settings: Settings = Provide[Container.settings]
+
+    ########################################################################
+    # forward
+    ########################################################################
 
     @du.recordlinkage_repeat
     def build_forward_indices(
